4813bc9eca59cfdb4ba9717de86b7879fe23606b
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-26 17:52:40 +0600

    created a new AllTasks component to display all lists

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
   <MainHeader title="Hello John" />
   <ButtonGeneric btn_text="This is a button" color="rgb(40,40,40)" />
+  <AllTasks :tasks="tasks" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { ButtonGeneric, MainHeader } from "./components";
+import { ButtonGeneric, MainHeader, AllTasks } from "./components";
 
 @Options({
   components: {
     MainHeader,
     ButtonGeneric,
+    AllTasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Hello",
+        day: "30th February, 2069",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "k thnx bye",
+        day: "30th February, 2069",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Hello",
+        day: "30th August, 2099",
+        reminder: true,
+      },
+    ];
   },
 })
 export default class App extends Vue {}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ButtonGeneric } from "./ButtonGeneric.vue";
 export { default as MainHeader } from "./MainHeader.vue";
+export { default as AllTasks } from "./AllTasks.vue";
