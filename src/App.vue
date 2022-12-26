163dcb1c17ddecbdb217e3e9ad28902aa4f218db
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-26 16:22:37 +0600

    First component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Hello World</h1>
+  <MainHeader />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import MainHeader from "./components/MainHeader.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    MainHeader,
   },
 })
 export default class App extends Vue {}
